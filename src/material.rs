004390672bc424d2018e263df3a76b69d1d0c73e
Gurjot Singh <53978284+gsingh003@users.noreply.github.com>
2025-08-13 19:13:44 -0600

    Remove unused imports

--- src/material.rs
+++ src/material.rs
@@ -1,8 +1,7 @@
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
-use crate::vec3::{Point3, Vec3};
+use crate::vec3::Vec3;
 use rand::Rng;
-use std::sync::Arc;
 
 pub trait Material: Send + Sync {
     fn scatter(
